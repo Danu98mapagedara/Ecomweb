236be9c2eb2550355b0f155682d319800d78b209
Dhanushka Mapagedara <mapagedaradlb.20@uom.lk>
2025-10-31 16:01:27 +0530

    update  forms with react hook form and zod validation

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export type ProductType = {
   id: string | number;
   name: string;
@@ -8,3 +10,39 @@ export type ProductType = {
   colors: string[];
   images: Record<string, string>;
 };
+
+export type ProductsType = ProductType[];
+
+export type CartItemType = ProductType & {
+  quantity: number;
+  selectedSize: string;
+  selectedColor: string;
+};
+
+export type CartItemsType = CartItemType[];
+
+
+
+export  const shippingFormSchema=z.object({
+  name:z.string().min(2,"Name  is required"),
+  email:z.email().min(1,"Invalid email address"),   
+  address:z.string().min(5,"Address is required"),
+  city:z.string().min(2,"City is required"),
+  phone:z.string().
+  min(7,"Phone is required")
+  .max(10,"Phone number must be 7-10 digits"),
+})
+export type ShippingFormInputs=z.infer<typeof shippingFormSchema>;
+
+
+export  const paymentFormSchema=z.object({
+  cardHolder:z.string().min(2,"Card Holder Name is required"),
+  cardNumber:z.
+  string()
+  .min(16,"Card Number must be 16 digits")
+  .max(16,"Card Number must be 16 digits"),
+  expirationDate:z.string().min(5,"Expiration Date is required"),
+  cvv:z.string().min(3,"CVV must be 3 digits").max(4,"CVV must be 3 or 4 digits"),
+})
+
+export type PaymentFormInputs=z.infer<typeof paymentFormSchema>;
